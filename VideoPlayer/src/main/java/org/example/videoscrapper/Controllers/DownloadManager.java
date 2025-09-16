4cccf89e04c1d602c6c9393e9eee4d42ccef0c7c
Kenric972 <kenric12356@gmail.com>
2025-09-16 10:04:24 -0500

    Delete VideoPlayer directory

--- VideoPlayer/src/main/java/org/example/videoscrapper/Controllers/DownloadManager.java
+++
@@ -1,9 +0,0 @@
-package org.example.videoscrapper.Controllers;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import org.example.videoscrapper.model.DownloadItem;
-
-public class DownloadManager {
-    public static final ObservableList<DownloadItem> downloads = FXCollections.observableArrayList();
-}
